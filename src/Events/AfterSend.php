1bc4f33988d9248be517992bdd5201400c96aaac
tekkenking <mytansill@yahoo.com>
2024-04-28 05:24:52 +0100

    aftersend event

---
+++ src/Events/AfterSend.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tekkenking\Swissecho\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class AfterSend
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public array $insightPayload, public $rawResponsePayload
+    ) {}
+}
