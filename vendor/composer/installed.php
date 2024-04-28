9875a9b8634b6ab04e9ffacb436fb57ef5d4a687
tekkenking <mytansill@yahoo.com>
2024-04-28 05:36:40 +0100

    ..

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tekkenking/swissecho',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '673ebaf6c7c5fd49ce00ae688412cf6fca781039',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tekkenking/swissecho' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '673ebaf6c7c5fd49ce00ae688412cf6fca781039',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
